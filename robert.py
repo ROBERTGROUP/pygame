2c4848d7512c13260d24d149b037bfc32e060d37
OLUPOTROBERT <robertolupot120@gmail.com>
2017-05-17 06:53:36 -0700

    robert.py

--- robert.py
+++ robert.py
@@ -53,7 +53,7 @@ while running:
     # 5 - clear the screen before drawing it again
     screen.fill(0)
     # 6 - draw the player on the screen at X:100, Y:100
-    for x in range(width/grass.get_width()+1):
+    for x in range(width//grass.get_width()+1):
         for y in range(height/grass.get_height()+1):
             screen.blit(grass,(x*100,y*100))
     screen.blit(castle,(0,30))
